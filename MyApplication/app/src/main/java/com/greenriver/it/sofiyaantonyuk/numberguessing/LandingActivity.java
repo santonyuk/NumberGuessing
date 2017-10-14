92b2e0ff0cde92d7abd7db713c39f89cccb27574
sofiedear <santonyuk2@mail.greenriver.edu>
2017-10-13 20:17:57 -0700

    created LandingActivity.java class

--- MyApplication/app/src/main/java/com/greenriver/it/sofiyaantonyuk/numberguessing/LandingActivity.java
+++ MyApplication/app/src/main/java/com/greenriver/it/sofiyaantonyuk/numberguessing/LandingActivity.java
@@ -1,13 +1,40 @@
 package com.greenriver.it.sofiyaantonyuk.numberguessing;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class LandingActivity extends AppCompatActivity {
+    LandingActivity activity;
+    Button startButton;
+    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing);
+
+        locateViews();
+        bindFunctionality();
     }
+
+    void bindFunctionality() {
+    startButton.setOnClickListener(new View.OnClickListener(){
+
+        @Override
+        public void onClick(View view) {
+            Intent intent = new Intent(activity.getApplicationContext(), GuessingActivity.class);
+            activity.startActivity(intent);
+        }
+    });
+    }
+
+    // locate views
+    void locateViews() {
+        startButton = (Button) findViewById(R.id.startButton);
+    }
+
+
 }
